65ec03998967824cc1233d240280b79434ae0307
Hatice Sarp <hatice.sarp@vlmedia.com.tr>
2026-02-13 14:59:40 +0300

    Initial Commit

---
+++ WeatherSampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherSampleApp
+//
+//  Created by Hatice Sarp on 13.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherSampleApp/WeatherSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSampleAppApp.swift
+//  WeatherSampleApp
+//
+//  Created by Hatice Sarp on 13.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
